e383b9dc3f3187ff1fcf7dc53ed1eef3c92fccd9
William Ritson <william.ritson@gmail.com>
2017-02-07 11:02:17 -0800

    Added in Treant based view

--- src/app/tree-view/tree-view.component.ts
+++ src/app/tree-view/tree-view.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef } from '@angular/core';
 import { Graph } from '../graph';
 import { DrawableTree, DrawTree, buchheim } from './draw-tree';
 import 'sigma';
+//import 'treant-js';
+
+declare var Treant: any;
+declare var treant: any;
+
 
 
 
@@ -11,57 +16,32 @@ import 'sigma';
   styleUrls: ['./tree-view.component.css']
 })
 export class TreeViewComponent implements OnInit {
-  sigma: SigmaJs.Sigma;
-  @Input() tree: DrawTree;
-  source: string;
-
-  constructor() {
-    let dt = new DrawableTree("A", [
-      new DrawableTree("B"),
-      new DrawableTree("C"),
-    ]);
-    console.log(dt);
-    let layout = buchheim(new DrawTree(dt));
-    console.log(layout);
-  }
-
-  refresh() {
-    this.sigma.graph.clear();
-    let edgeCount = 0;
-    // Add Nodes
-    /*
-    for (let nodeName in this.graph.nodes) {
-      let node = this.graph.nodes[nodeName];
-      this.sigma.graph.addNode({
-        id: nodeName,
-        label: node.data,
-        size: 1,
-        x: Math.random() * 280,
-        y: Math.random() * 280,
-        color: node.colorStr()n
-      });
-    }
-    // Add Edges
-    for (let nodeName in this.graph.nodes) {
-      let node = this.graph.nodes[nodeName];
-      for (let link in node.links) {
-        this.sigma.graph.addEdge({
-          id: 'e' + edgeCount,
-          source: nodeName,
-          target: link,
-          type: "arrow",
-          head: "arrow"
-        });
-        edgeCount++;
-      }
-    }
-    */
-    this.sigma.refresh();
-  }
-
+  constructor(private el: ElementRef) {}
 
   ngOnInit() {
-    this.sigma = new sigma('tree-render-target');
+    console.log('mello');
+    console.log(Treant);
+
+    let simple_chart_config = {
+      chart: {
+        container: "#tree-render-target"
+      },
+
+      nodeStructure: { 
+        text: { name: "Parent node" },
+        children: [
+          {
+            text: { name: "First child" }
+          },
+          {
+            text: { name: "Second child" }
+          }
+        ]
+      }
+    };
+
+    let my_chart = new Treant(simple_chart_config);
   }
+
 
 }
